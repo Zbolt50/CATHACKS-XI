93f72dabd5265e09b264781461d02a8c9c3e2dec
Zbolt50 <zekeboggs10@gmail.com>
2025-04-05 01:52:55 -0400

    pushing tile_map stuff

--- assets/tiles/cathacks_tiles.tsx
+++ assets/tiles/cathacks_tiles.tsx
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="cathacks_tiles" tilewidth="32" tileheight="32" tilecount="19" columns="0">
+ <tileoffset x="16" y="16"/>
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="58">
   <image source="pngs/ground_grass_plain.png" width="32" height="32"/>
